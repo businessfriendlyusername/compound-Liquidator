0b88f5f5c27bc66b624fdb896688dfb3e44fed0c
grandwizard420 <sluttmuffinz69@gmail.com>
2020-01-21 22:06:24 -0800

    added initialization

--- liquidation watcher/init.js
+++ liquidation watcher/init.js
@@ -1,1 +1,27 @@
 const fs = require('fs')
+const logging = require('./logging')
+const Web3 = require('web3')
+const net = require('net')
+
+
+const config = JSON.parse(fs.readFileSync(__dirname + "/config.json", "utf8"))
+
+const loggerOutFile = config.logToFile ? config.logFile : false
+const log = new logging(
+  outputPath=loggerOutFile, 
+  console=config.logToConsole, 
+  verbosity=config.loggingVerbosity)
+
+log.info(`Logger initialized with verbosity: ${config.loggingVerbosity}`);
+log.info(`Logger outputting to: ${config.logFile}`);
+
+
+const provider = config.useIPC ? new Web3.providers.IpcProvider(config.IPC, net) : new Web3(config.WS, net)
+const web3 = new Web3(provider)
+
+module.exports = {
+  log,
+  web3,
+  config,
+  provider
+}
